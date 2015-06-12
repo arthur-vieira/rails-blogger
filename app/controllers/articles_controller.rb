c0dcc55e96fd1c56fe294c7d9e3349afd56d1104
Arthur Vieira <arthur.apv@gmail.com>
2015-06-12 04:42:51 +0000

    bug fixed in articles#destroy

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -22,7 +22,8 @@ class ArticlesController < ApplicationController
 		redirect_to article_path(@article)
 	end
 	def destroy
-		Article.find(params[:id]).destroy
+		@article = Article.find(params[:id])
+		@article.destroy
 		flash.notice = "Article '#{@article.title}' Deleted!"
 		redirect_to articles_path
 	end
