9fd6e8de970502f2de7718f5e5d43db2473a24ef
Arthur Vieira <arthur.apv@gmail.com>
2015-07-01 20:33:21 +0000

    added paperclip gem to provide attachment functionality to articles

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,6 +2,13 @@ class Article < ActiveRecord::Base
 	has_many :comments
   has_many :taggings
   has_many :tags, through: :taggings
+  #has_attached_file :image # this method is part of the paperclip library.
+  # paperclip will understand that this model should accept a file attachment and that there are fields to store information about that file which start with image_ in this model’s database table
+  has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
+
+  #validates_attachment_content_type :image, :content_type => ["image/jpg", "image/jpeg", "image/png"] # Paperclip raises MissingRequiredValidatorError error if you do not do this
+  # As of version 4.0, for security reasons, all attachments are required to include a content_type validation, a file_name validation, or to explicitly state that they’re not going to have either.
+  validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
 
   def tag_list
     #tags.join(", ") # doesn't work 'cause the array of tags is an Array of tag instances. To make it work, either add a "def to_s; name; end" method to tag.rb or use the method below:
